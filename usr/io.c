c96a8d40db764b1cf06d6623af7ec68a89fc0e71
Daiyuu Nobori <dnobori.gitcommit@coe.ad.jp>
2024-01-22 00:59:19 +0900

    aaaa

--- usr/io.c
+++ usr/io.c
@@ -596,7 +596,7 @@ iscsi_io_send_pdu(iscsi_conn_t *conn,
 
 	if (session->use_ipc)
 		ipc->send_pdu_begin(session->t->handle, session->id,
-				conn->id, 0,
+				conn->id, 48,
 				(sizeof(*hdr) + hdr->hlength) + (ntoh24(hdr->dlength) + pad_bytes)
 		);
 
