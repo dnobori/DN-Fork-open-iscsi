534722ee12e136fa66abbb0836c8ed99613e1546
Daiyuu Nobori <dnobori.gitcommit@coe.ad.jp>
2024-01-22 00:10:39 +0900

    a

--- usr/io.c
+++ usr/io.c
@@ -597,11 +597,11 @@ iscsi_io_send_pdu(iscsi_conn_t *conn,
 	if (session->use_ipc)
 		ipc->send_pdu_begin(session->t->handle, session->id,
 				    conn->id, (sizeof(*hdr) + hdr->hlength) + (ntoh24(hdr->dlength) + pad_bytes),
-			4
+			0
 				    //ntoh24(hdr->dlength) + pad_bytes
 		);
 
-	int tmpbuf_size = sizeof(*hdr) + hdr->hlength + ntoh24(hdr->dlength) + pad_bytes + 4;
+	int tmpbuf_size = sizeof(*hdr) + hdr->hlength + ntoh24(hdr->dlength) + pad_bytes + 0;
 
 	tmpbuf = malloc(tmpbuf_size);
 
